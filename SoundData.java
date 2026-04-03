238b5b916d8c8a5618cf746fe143013a90ce55b6
CodeMasterPrime <nettapro2@gmail.com>
2026-04-03 17:06:43 +0700

    Add SoundData class for sound measurement

---
+++ SoundData.java
@@ -0,0 +1,10 @@
+package com.example.soundmeter;
+public class SoundData {
+    public long time;
+    public float value;
+
+    public SoundData(float v, long t) {
+        value = v;
+        time = t;
+    }
+}
